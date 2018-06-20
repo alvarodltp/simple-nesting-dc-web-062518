17559ab60ed434da5bb17636b2cf3b3551c4eb9e
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-20 17:40:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
      }
 
    programmer_hassh[:yukihiro_matsumoto] 
-   :yukihiro_matsumoto = [:known_for =>"Ruby", :languages =>["LISP", "C"]]
+   [:yukihiro_matsumoto] = {:known_for =>"Ruby", :languages =>["LISP", "C"]}
    
 end
 
