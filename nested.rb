ce826162022a9387f165ce25afa8ad0b3fdba4df
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-20 17:12:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,8 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_gash[:alan_kay][:known_for]
+programmer_gash[:alan_kay][:known_for]
+
 	
 	programmer_hash = 
  		{
